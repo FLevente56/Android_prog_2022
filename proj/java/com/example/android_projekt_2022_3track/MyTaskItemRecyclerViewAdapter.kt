4b638407d4c9eb904408e638e0b195e1f27f902f
FLevente56 <64910921+FLevente56@users.noreply.github.com>
2022-12-30 17:06:14 +0200

    Add files via upload

---
+++ proj/java/com/example/android_projekt_2022_3track/MyTaskItemRecyclerViewAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.android_projekt_2022_3track
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.example.android_projekt_2022_3track.placeholder.PlaceholderContent.PlaceholderItem
+import com.example.android_projekt_2022_3track.databinding.FragmentTaskItemBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MyTaskItemRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>,
+) : RecyclerView.Adapter<MyTaskItemRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(FragmentTaskItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent,
+            false))
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentTaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
